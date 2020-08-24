13dbda277d812f644b3a0d657b27f3a9f571816c
MARCIO OHIRA <005918958@itaud.des.ihf>
2020-08-24 15:24:58 -0300

    versionando home action

--- src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
+++ src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
@@ -18,5 +18,9 @@ public class ValidadorController {
 		}
 		 return "{\"status\": \"invalido\"}";
 	}
-
+	
+	@GetMapping("/")
+	public String index() {
+		 return "{\"mensagem\": \"api itau turma 7c\"}";
+	}
 }
